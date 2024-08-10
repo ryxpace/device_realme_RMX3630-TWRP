c8d2337dbb54842bac4533b682e1313a6f3fc442
ryxpace <raydawn322@gmail.com>
2024-08-10 09:10:35 +0900

    RE8DDCL1: refactoring

--- twrp_RE8DDCL1.mk
+++ twrp_RE8DDCL1.mk
@@ -9,17 +9,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from ossi device
-$(call inherit-product, device/oplus/ossi/device.mk)
+$(call inherit-product, device/realme/RE8DDCL1/device.mk)
 
-PRODUCT_DEVICE := ossi
-PRODUCT_NAME := omni_ossi
-PRODUCT_BRAND := oplus
-PRODUCT_MODEL := ossi
-PRODUCT_MANUFACTURER := oplus
+PRODUCT_DEVICE := RE8DDCL1
+PRODUCT_NAME := twrp_RE8DDCL1
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3630
+PRODUCT_MANUFACTURER := realme
 
 PRODUCT_GMS_CLIENTID_BASE := android-oplus
 
